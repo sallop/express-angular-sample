109ac56f35f4227686c52f41b9d78924e49b71f0
sallop <shiogai1987@yahoo.co.jp>
2016-01-10 02:16:45 +0900

    remove comment and garbage

--- src/app.js
+++ src/app.js
@@ -11,90 +11,16 @@ app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 app.use(express.static('public'));
 app.use('/bower_components', express.static(__dirname + '/../bower_components'));
-//app.use( express.bodyParser() );
-//app.use( bodyParser() );
-//app.set('view options', { layout: false });
-
-//app.get('/', function(req, res){
-//	res.send("This would be some HTML");
-//});
 
 app.get('/', function(req, res){
-	//res.sendfile('./views/index.html');
 	res.sendFile(__dirname + '/views/index.html');
-	//res.render('index', {
-	//	title  : "Things you own end up own you.",
-	//	entries: blogEngine.getBlogEntries()
-	//});
-	//
-	//
-	//var connection = mysql.createConnection({
-	//	host: 'localhost',
-	//	user: 'tycc',
-	//	database: 'list_of_church',
-	//	password: ''
-	//});
-
-	//connection.connect();
-
-	//connection.query('SELECT fullname, birthday FROM list;', function (err, rows, fields){
-	//	if(err) throw err;
-	//	console.log( rows );
-	//	//console.log( fields );
-	//});
-
-	//connection.query('SELECT * FROM list WHERE id = ' + connection.escape(310), function(err, results){
-	//connection.query('SELECT * FROM list WHERE id = ?', [310], function( err, results ){
-	//var query = connection.query('INSERT INTO list SET ?', {
-	//	id: 310,
-	//	christian_name: 'isidoro',
-	//	fullname: '',
-	//	birthday: '1987-07-07'
-	//}, function( err, results ){
-	//	console.log( results );
-	//});
-	//console.log( query );
-
-	//query = connection.query('SELECT * FROM list WHERE team = ?', [6], function( err, result ){
-	//	console.log("SELECT * FROM list WHERE team = ?");
-	//	console.log( result );
-	//	//res.send( entries );
-	//	//res.render('list', { entries: result}); // for .hbs
-	//	//res.send('index'); // for .hbs
-	//	res.sendFile(__dirname + '/views/index.html'); // for .hbs
-	//	//for(var i in result){
-	//	//	res.render('list', result);
-	//	//	console.log(result[i])
-	//	//}
-
-	//});
-
-	//query = connection.query('DELETE FROM list WHERE id = 310', function (err, result){
-	//	if (err) throw err;
-	//kk
-	//	console.log('deleted ' + result.affectedRows + ' rows');
-	//});
-
-	//var sql = "SELECT * FROM ?? WHERE ?? = ?";
-	//var inserts = ['users', 'id', 310];
-	//sql = mysql.format(sql, inserts);
-	//console.log( sql );
-
-	//connection.end();
-
 });
 
 app.get('/about', function(req, res){
-	//res.sendfile('./views/about.html');
-	//res.sendFile(__dirname + '/views/about.html');
-	//res.render('about', { title: "About Me", layout: 'layout' });
 	res.render('layout', { title: "About Me" });
 });
 
 app.get('/article/:id', function(req, res){
-	//res.sendfile('./views/article.html');
-	//res.sendFile(__dirname + '/views/article.html');
-	//res.render('article');
 	var entry = blogEngine.getBlogEntry(req.params.id);
 	res.render('article', { title: entry.title, blog:entry });
 });
@@ -112,55 +38,16 @@ app.get('/list/:team', function(req, res){
 
 	connection.connect();
 
-	//connection.query('SELECT fullname, birthday FROM list;', function (err, rows, fields){
-	//	if(err) throw err;
-	//	console.log( rows );
-	//	//console.log( fields );
-	//});
-
-	//connection.query('SELECT * FROM list WHERE id = ' + connection.escape(310), function(err, results){
-	//connection.query('SELECT * FROM list WHERE id = ?', [310], function( err, results ){
-	//var query = connection.query('INSERT INTO list SET ?', {
-	//	id: 310,
-	//	christian_name: 'isidoro',
-	//	fullname: '',
-	//	birthday: '1987-07-07'
-	//}, function( err, results ){
-	//	console.log( results );
-	//});
-	//console.log( query );
-
 	query = connection.query('SELECT * FROM list WHERE team = ?', [req.params.team], function( err, result ){
 		console.log("SELECT * FROM list WHERE team = ?");
 		console.log( result );
-		//res.send( entries );
-		//res.render('list', { entries: result}); // for .hbs
-		//res.send('index'); // for .hbs
-		//res.sendFile(__dirname + '/views/index.html'); // for .hbs
-		//for(var i in result){
-		//	res.render('list', result);
-		//	console.log(result[i])
-		//}
 		res.json( result );
 
 	});
 
-	//query = connection.query('DELETE FROM list WHERE id = 310', function (err, result){
-	//	if (err) throw err;
-	//kk
-	//	console.log('deleted ' + result.affectedRows + ' rows');
-	//});
-
-	//var sql = "SELECT * FROM ?? WHERE ?? = ?";
-	//var inserts = ['users', 'id', 310];
-	//sql = mysql.format(sql, inserts);
-	//console.log( sql );
-
 	connection.end();
 });
 
-//app.post('/insert', function( req, res ){
-//app.get('/insert/:fullname', function( req, res ){
 app.get('/insert/:id/:team/:fullname/:christian_name/:birthday/:telephone/:postcode/:address', function( req, res ){
 	var connection = mysql.createConnection({
 		host: 'localhost',
@@ -168,11 +55,7 @@ app.get('/insert/:id/:team/:fullname/:christian_name/:birthday/:telephone/:postc
 		database: 'list_of_church',
 		password: ''
 	});
-	//var sql = "INSERT INTO list (id, team, fullname, christian_name) VALUE (?,?,?,?);";
-	//var sql = "INSERT INTO list (id, team, fullname, christian_name) VALUE (?,?,?,?);";
 	var sql = "INSERT INTO list (id, team, fullname, christian_name, birthday, telephone, postcode, address) VALUE (?,?,?,?,?,?,?,?);";
-	//var inserts = ['7', '', 'isidoro'];
-	//var inserts = ['707', 7, req.params.fullname, 'isidoro'];
 	var inserts = [
 		req.params.id,
 		req.params.team,
@@ -189,9 +72,7 @@ app.get('/insert/:id/:team/:fullname/:christian_name/:birthday/:telephone/:postc
 	console.log( sql );
 	connection.connect();
 
-	//query = connection.query('INSERT INTO list SET ?',
 	query = connection.query( sql, function( err, result ){
-		//res.render('list', { entries: result });
 		res.json( result );
 		console.log( result );
 	});
@@ -200,7 +81,6 @@ app.get('/insert/:id/:team/:fullname/:christian_name/:birthday/:telephone/:postc
 
 });
 
-//app.get('/insert/:fullname/:christian_name', function( req, res ){
 app.get('/insert', function( req, res ){
 	var connection = mysql.createConnection({
 		host: 'localhost',
@@ -213,17 +93,13 @@ app.get('/insert', function( req, res ){
 	var fullname = 'John Smith';
 	var christian_name = 'St.Expedite';
 	var sql = "INSERT INTO list (id, team, fullname, christian_name) VALUE (?,?,?,?);";
-	//var inserts = ['7', 'John Smith', 'isidoro'];
-	//var inserts = ['7', req.params.fullname, req.params.christian_name];
 	var inserts = [id, team, fullname, christian_name];
 	sql = mysql.format( sql, inserts );
 
 	console.log( sql );
 	connection.connect();
 
-	//query = connection.query('INSERT INTO list SET ?',
 	query = connection.query( sql, function( err, result ){
-		//res.render('list', { entries: result });
 		console.log( result );
 	});
 
@@ -232,12 +108,7 @@ app.get('/insert', function( req, res ){
 });
 
 app.get('/read/:id/:team/:fullname/:christian_name/:birthday/:telephone/:postcode/:address', function( req, res ){
-	// req.query.id
-	// req.query.fullname
-	// /read?id=404&fullname=tyler+durden
-	//var sql = "SELECT * FROM list WHERE;";
 	var sql = "?,?,?,?,?,?,?,?";
-	//var inserts = ['7', req.params.fullname, req.params.christian_name];
 	var params = [
 		req.params.id,
 		req.params.team,
@@ -253,20 +124,11 @@ app.get('/read/:id/:team/:fullname/:christian_name/:birthday/:telephone/:postcod
 	console.log( sql );
 });
 
-
-
 app.get('/api/users', function(req, res){
-	//var user_id = req.param('id');
-	//var token = req.param('token');
-	//var geo = req.param('geo');
-
 	var user_id = req.param.id;
 	var token = req.param.token;
 	var geo = req.param.geo;
-
-
 	res.send( user_id + ' ' + token + ' ' + geo);
 });
-
 
 app.listen(3000);
