2dc190700b5eb29c263e04afb2b66cbe82d21aa8
sallop <shiogai1987@yahoo.co.jp>
2016-01-18 19:38:40 +0900

    some change

--- src/public/javascripts/controller.js
+++ src/public/javascripts/controller.js
@@ -1,4 +1,4 @@
-var listApp = angular.module('listApp', []);
+var listApp = angular.module('listApp', ['ui.bootstrap']);
 
 listApp.controller('listCtrl', function($scope, $http){
     $scope.click = function(n){
@@ -104,4 +104,50 @@ listApp.controller('listCtrl', function($scope, $http){
       console.log("error occured");
     });
   };
+}).controller('ModalDemoCtrl', function ($scope, $uibModal, $log) {
+
+    $scope.items = ['item1', 'item2', 'item3'];
+
+      $scope.animationsEnabled = true;
+
+        $scope.open = function (size) {
+
+              var modalInstance = $uibModal.open({
+                      animation: $scope.animationsEnabled,
+                        templateUrl: 'myModalContent.html',
+                        controller: 'ModalInstanceCtrl',
+                        size: size,
+                        resolve: {
+                                  items: function () {
+                                              return $scope.items;
+                                                      }
+                                               }
+                  });
+
+                  modalInstance.result.then(function (selectedItem) {
+                          $scope.selected = selectedItem;
+                              }, function () {
+                                      $log.info('Modal dismissed at: ' + new Date());
+                                          });
+                    };
+
+          $scope.toggleAnimation = function () {
+                $scope.animationsEnabled = !$scope.animationsEnabled;
+                  };
+
+}).controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+  $scope.items = items;
+    $scope.selected = {
+      item: $scope.items[0]
+    };
+
+  $scope.ok = function () {
+    $uibModalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
 });
+
